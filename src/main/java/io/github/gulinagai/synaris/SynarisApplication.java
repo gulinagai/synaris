de8e313e247100b1b693788201139b3a6c9d408b
Gustavo Nagai Amorim <gulifiree@gmail.com>
2026-06-30 23:36:29 -0300

    Inicilizando projeto Spring Boot - Configurações iniciais

---
+++ src/main/java/io/github/gulinagai/synaris/SynarisApplication.java
@@ -0,0 +1,13 @@
+package io.github.gulinagai.synaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynarisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gulinagai/synaris/SynarisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gulinagai.synaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
