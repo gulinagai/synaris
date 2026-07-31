1489ddd39f247fdb3480e00613c1d2dfbca507c7
Gustavo Nagai Amorim <gulifiree@gmail.com>
2026-07-30 22:59:27 -0300

    feat(pacientes): implementa Value Object Cpf

--- src/main/java/io/github/gulinagai/synaris/paciente/domain/model/Cpf.java
+++ src/main/java/io/github/gulinagai/synaris/paciente/domain/model/Cpf.java
@@ -1,4 +1,45 @@
 package io.github.gulinagai.synaris.paciente.domain.model;
 
-public class Cpf {
+import java.util.Objects;
+
+public final class Cpf {
+
+    private final String valor;
+
+    public Cpf(String valor) {
+        Objects.requireNonNull(valor, "CPF não pode ser nulo.");
+
+        valor = valor.replaceAll("\\D", "");
+
+        if(valor.isEmpty()) {
+            throw new IllegalArgumentException("CPF não pode ser vazio.");
+        }
+
+        if(valor.length() != 11) {
+            throw  new IllegalArgumentException("CPF deve conter 11 dígitos.");
+        }
+
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(!(o instanceof Cpf cpf)) return false;
+        return Objects.equals(valor, cpf.valor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(valor);
+    }
+
+    @Override
+    public String toString() {
+        return valor;
+    }
 }
